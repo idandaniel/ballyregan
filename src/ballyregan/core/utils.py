8908ebc543555b573058e0376de20f126fefe830
idandaniel <idandaniel12@gmail.com>
2022-11-19 23:27:04 +0200

    removed unused function from utils

--- src/ballyregan/core/utils.py
+++ src/ballyregan/core/utils.py
@@ -6,19 +6,6 @@ from typing import List
 from ballyregan import Proxy
 
 
-def split_list_to_chunks(lst: list, chunks_count: int) -> List[list]:
-    """Gets a list and return the list splitted to [chunks_amount] chunks
-
-    Args:
-        lst (list): The list to split
-        chunks_count (int): The amount of chunk the list will be splitteed into.
-
-    Returns:
-        List[list]: List of [chunks_amount] lists.
-    """
-    return [lst[i::chunks_count] for i in range(chunks_count)]
-
-
 def make_requests_proxies_dict_from_proxy(proxy: Proxy) -> dict:
     """takes a proxy model and returns a dict that requests package is able to use as 'proxies' is request
 
